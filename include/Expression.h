5357f68d5c91c707aaaeae7002e4bfd4a0ea1b71
MortalFlame21 <markgio2100@gmail.com>
2025-07-23 15:36:26 +1000

    Edit Expression.h. Revise awful std::unique_ptr decisions. -_-.

--- include/Expression.h
+++ include/Expression.h
@@ -17,19 +17,24 @@ public:
 class Expression {
 public:
     Expression() = default;
-    virtual ~Expression();
-	virtual std::any accept(Visitor& visitor);
+    virtual ~Expression() { };
+
+    virtual std::any accept(Visitor& visitor) { return visitor.visitBinary(); };
 };
 
 class Binary : public Expression {
 public:
-    Binary(Expression left, Expression right, Token operator_)
-        : m_left{ std::make_unique<Expression>(left) }
-        , m_right{ std::make_unique<Expression>(right)  }
+    Binary (
+        std::unique_ptr<Expression> left,
+        std::unique_ptr<Expression> right,
+        Token operator_
+    )
+        : m_left{ std::move(left) }
+        , m_right{ std::move(right)  }
         , m_operator{ operator_ }
     { }
 
-    std::any accept(Visitor& visitor) {
+    std::any accept(Visitor& visitor) override {
         return visitor.visitBinary();
     }
 private:
@@ -40,25 +45,36 @@ private:
 
 class Grouping : public Expression {
 public:
-    Grouping(Expression expression)
-        : m_expression{ std::make_unique<Expression>(expression) }
+    Grouping(std::unique_ptr<Expression> expression)
+        : m_expression{ std::move(expression) }
     { }
 
-    std::any accept(Visitor& visitor) {
+    std::any accept(Visitor& visitor) override {
         return visitor.visitGrouping();
     }
 private:
     std::unique_ptr<Expression> m_expression;
 };
 
+class Literal : public Expression {
+public:
+    Literal(std::any value) : m_value { value } {}
+
+    std::any accept(Visitor& visitor) override {
+        return visitor.visitGrouping();
+    }
+private:
+    std::any m_value;
+};
+
 class Unary : public Expression {
 public:
-    Unary(Expression right, Token operator_)
-        : m_right{ std::make_unique<Expression>(right) }
+    Unary(std::unique_ptr<Expression> right, Token operator_)
+        : m_right{ std::move(right) }
         , m_operator{ operator_ }
     { }
 
-    std::any accept(Visitor& visitor) {
+    std::any accept(Visitor& visitor) override {
         return visitor.visitUnary();
     }
 private:
