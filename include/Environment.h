794eb2a40b60f1ab9b5e40fa398d677247172b55
MortalFlame21 <markgio2100@gmail.com>
2025-11-12 14:24:05 +1100

    Environment.h. Split Environment def & decl.

--- include/Environment.h
+++ include/Environment.h
@@ -4,19 +4,14 @@
 #include <string_view>
 #include <any>
 
-#include "Interpreter.h"
+class Token;
+class Interpreter;
 
 class Environment {
 private:
-    void define(std::string_view name, std::any& value) {
-        m_values.insert({ name, value });
-    }
+    void define(std::string_view name, std::any& value);
 
-    std::any get(Token name) {
-        if (m_values.find(name.m_lexeme) != m_values.end())
-            return m_values.find(name.m_lexeme);
-        throw Interpreter::RuntimeError(name, "Undefined variable " + name.m_lexeme);
-    }
+    std::any get(Token name);
 
     std::unordered_map<std::string_view, std::any> m_values {};
 };
---
+++ src/Environment.cpp
@@ -0,0 +1,13 @@
+#include "Environment.h"
+#include "Lexer.h"
+#include "Interpreter.h"
+
+void Environment::define(std::string_view name, std::any& value) {
+    m_values.insert({ name, value });
+}
+
+std::any Environment::get(Token name) {
+    if (m_values.find(name.m_lexeme) != m_values.end())
+        return m_values.find(name.m_lexeme);
+    throw Interpreter::RuntimeError(name, "Undefined variable " + name.m_lexeme);
+}
