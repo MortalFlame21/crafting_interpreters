606858952454fa6b741c0d6b7428140fb8ea7fb8
MortalFlame21 <markgio2100@gmail.com>
2025-11-12 20:22:14 +1100

    Environment.cpp. get. Return value.

--- src/Environment.cpp
+++ src/Environment.cpp
@@ -8,6 +8,6 @@ void Environment::define(std::string_view name, std::any& value) {
 
 std::any Environment::get(Token name) {
     if (m_values.find(name.m_lexeme) != m_values.end())
-        return m_values.find(name.m_lexeme);
+        return m_values.find(name.m_lexeme)->second;
     throw Interpreter::RuntimeError(name, "Undefined variable " + name.m_lexeme);
 }
