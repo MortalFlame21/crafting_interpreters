120f6ee575f5d15c221405697eb69210026cb0a3
MortalFlame21 <markgio2100@gmail.com>
2025-11-13 23:00:53 +1100

    Expression.h. Derive Assignment.

--- include/Expression.h
+++ include/Expression.h
@@ -26,6 +26,7 @@ public:
         virtual std::any visitLiteral(const Literal& literal) = 0;
         virtual std::any visitUnary(const Unary& unary) = 0;
         virtual std::any visitVariable(const Variable& variable) = 0;
+        virtual std::any visitVariable(const Variable& variable) = 0;
     };
 
     virtual std::any accept(Visitor& visitor) = 0;
@@ -123,11 +124,30 @@ public:
     virtual ~Variable() { };
 
     std::any accept(Visitor& visitor) override {
-        return visitor.visitVariable(*this);
+        // return visitor.visitVariable(*this);
     }
 
     friend class AstPrinter;
     friend class Interpreter;
 private:
     Token m_name;
+};
+
+class Assignment : public Expression {
+public:
+    Assignment(Token name, std::unique_ptr<Expression> value)
+        : m_name{ name }, m_value{ std::move(value) }
+    { }
+
+    virtual ~Assignment() { };
+
+    std::any accept(Visitor& visitor) override {
+        return "visitor.visitVariable(*this)";
+    }
+
+    friend class AstPrinter;
+    friend class Interpreter;
+private:
+    Token m_name;
+    std::unique_ptr<Expression> m_value;
 };
