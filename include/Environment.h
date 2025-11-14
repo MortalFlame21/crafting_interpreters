c74a4b9efd0d3e61f7cd0af41e4a83e75dab165a
MortalFlame21 <markgio2100@gmail.com>
2025-11-15 00:08:53 +1100

    Environment.h. Recursive class for enclosing block scope.

--- include/Environment.h
+++ include/Environment.h
@@ -2,6 +2,7 @@
 
 #include <unordered_map>
 #include <string>
+#include <memory>
 #include <any>
 
 class Token;
@@ -9,9 +10,15 @@ class Interpreter;
 
 class Environment {
 public:
+    Environment() { }
+    Environment(std::unique_ptr<Environment> enclosing)
+        : m_enclosing { std::move(enclosing) }
+    { }
+
     void define(const std::string& name, std::any& value);
     std::any get(Token name);
     void assign(Token name, std::any& value);
 private:
     std::unordered_map<std::string, std::any> m_values {};
+    std::unique_ptr<Environment> m_enclosing {};
 };
