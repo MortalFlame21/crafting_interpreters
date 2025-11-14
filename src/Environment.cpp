ed1a83ef3fb52b0f39361a2d483d3fb6903bfe49
MortalFlame21 <markgio2100@gmail.com>
2025-11-15 00:13:44 +1100

    Environment.cpp. get & assign. Add chain traversal.

--- src/Environment.cpp
+++ src/Environment.cpp
@@ -9,6 +9,10 @@ void Environment::define(const std::string& name, std::any& value) {
 std::any Environment::get(Token name) {
     if (m_values.find(name.m_lexeme) != m_values.end())
         return m_values.find(name.m_lexeme)->second;
+
+    // walk up the chain to find variable with same name
+    if (m_enclosing) return m_enclosing->get(name);
+
     throw Interpreter::RuntimeError(name, "Undefined variable " + name.m_lexeme);
 }
 
@@ -17,5 +21,12 @@ void Environment::assign(Token name, std::any& value) {
         m_values.at(name.m_lexeme) = value;
         return;
     }
+
+    // walk up the chain to find variable with same name
+    if (m_enclosing) {
+        m_enclosing->assign(name, value);
+        return;
+    }
+
     throw Interpreter::RuntimeError(name, "Undefined variable " + name.m_lexeme);
 }
