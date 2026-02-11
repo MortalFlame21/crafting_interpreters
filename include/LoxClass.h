26da5defd017f019dd050d4d55391e5ade7ebf9a
MortalFlame21 <markgio2100@gmail.com>
2026-02-11 21:50:31 +1100

    LoxClass.h. Add LoxInstance class.

--- include/LoxClass.h
+++ include/LoxClass.h
@@ -5,13 +5,15 @@
 
 #include "Callable.h"
 
+class LoxInstance;
+
 class LoxClass : public Callable {
 public:
     LoxClass(std::string name)
         : m_name { name } { }
 
     std::ostream& operator<<(std::ostream& out) {
-        return out << "class <" << m_name << ">";
+        return out << "class<" << m_name << ">";
     }
 
     std::any call (
@@ -20,6 +22,20 @@ public:
     ) override;
     std::size_t arity() override;
     std::string str() override;
+
+    friend class LoxInstance;
 private:
     std::string m_name;
+};
+
+class LoxInstance {
+public:
+    LoxInstance(LoxClass* class_)
+        : m_class { std::shared_ptr<LoxClass>(class_) } { }
+
+private:
+    std::shared_ptr<LoxClass> m_class { };
+    std::ostream& operator<<(std::ostream& out) {
+        return out << "classInstance<" << m_class->m_name << ">";
+    }
 };
